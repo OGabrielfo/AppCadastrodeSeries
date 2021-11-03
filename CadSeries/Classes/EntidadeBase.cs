ec00f40f8bb6d9ff99115bb04a448b182929cfd3
O Gabrielfo <ogabrielfo.silva@gmail.com>
2021-11-03 16:39:17 -0300

    Initial commit

---
+++ CadSeries/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace CadSeries
+{
+    public class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ CadSeries/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CadSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadeBase minhaClasse = new EntidadeBase();
+            Serie meuObjeto = new Serie();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
